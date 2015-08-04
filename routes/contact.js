f890b3bf8ab80ae5191cb36c4d55447ba2deb385
calebgregory <calebgregory@gmail.com>
2015-08-04 16:06:03 -0500

    Adding a way to anonymously send emails using nodemailer-direct-transport

--- routes/contact.js
+++ routes/contact.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var contact = express.Router();
 var nodemailer = require('nodemailer');
+var directTransport = require('nodemailer-direct-transport');
 
 contact.get('/', function(req,res){
   var attr = {
@@ -12,21 +13,21 @@ contact.get('/', function(req,res){
 
 contact.post('/email', function(req,res){
   var d = req.body,
-      transporter = nodemailer.createTransport({
-    service : 'Gmail',
-    auth : {
-      user : 'fuzzydicetroll666@gmail.com',
-      pass : '[password]'
-    }
-  });
+      transporter = nodemailer.createTransport(directTransport());
   transporter.sendMail({
     from    : d.email,
     to      : 'fuzzydicetroll666@gmail.com',
     subject : 'message from ['+ d.name+ ']',
     text    : 'from: ['+ d.name+ '] <'+ d.email+ '>\n'+
               'message: '+ d.message
+  },function(err,info) {
+    if (err) {
+      console.log(err);
+      res.send('email didn\'t send');
+    } else {
+      res.send('thanks for the email!');
+    }
   });
-  res.send('thanks for the email!');
 });
 
 module.exports = contact;
