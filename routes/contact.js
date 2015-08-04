881b208bf86ddc4834d3ddad036341b049d2907f
calebgregory <calebgregory@gmail.com>
2015-08-04 14:55:17 -0500

    Adding form for contact page and 'POST' handler for the contact route

--- routes/contact.js
+++ routes/contact.js
@@ -9,4 +9,9 @@ contact.get('/', function(req,res){
   res.render('templates/contact',attr);
 });
 
+contact.post('/email', function(req,res){
+  console.log(req.body);
+  res.send('yes');
+});
+
 module.exports = contact;
